b41d15a678819208afbe04492c50cff59123258a
Harald Berthelsen <harald@stts.se>
2017-03-13 12:47:13 +0100

    changed to handle sentence-internal silence

--- egs/slt_arctic/s1/scripts/addProminenceValues.py
+++ egs/slt_arctic/s1/scripts/addProminenceValues.py
@@ -40,15 +40,17 @@ def addProm(txtfile):
 
 
     #Add the initial silence token to texttokens and promvalues
-    texttokens.insert(0, "initial_silence")
-    promvalues.insert(0, "0")
+    #texttokens.insert(0, "initial_silence")
+    #promvalues.insert(0, "0")
     
     #Add the final silence token to texttokens and promvalues
     #IF the last token isn't punctuation
     #if not re.match("[.,?!:;]", texttokens[-1]):
-    texttokens.append("final_silence")
-    promvalues.append("0")
+    #texttokens.append("final_silence")
+    #promvalues.append("0")
 
+    #The "nr" is x if the token is a silence
+    
         
     #Now read prompt-lab, append prom values, and print it back
     promptlabfile = os.path.join(promptlabdir,basename+".lab")
@@ -69,17 +71,28 @@ def addProm(txtfile):
         nr = m.group(2)
         if nr != prev:
             prev = nr
-            i += 1
+            if nr == "x":
+                promvalue = "0"
+            else:
+                i += 1
+                promvalue = promvalues[i]
             print("NEW TOKEN ID: %s, index: %d" % (nr, i))
+
+        if nr == "x":
+            promvalue = "0"
+        else:
+            promvalue = promvalues[i]
+
+            
         #add prom before state if it exists
         m2 = re.search("^(.+)(\[[0-9]+\])$", pl_line)
         if m2:
             first = m2.group(1)
             last = m2.group(2)
-            print("Adding '%s: %s' to label file, symbol: %s" % (texttokens[i],promvalues[i], symbol))
-            new_pl_line = first+"/K:"+promvalues[i]+last
+            print("Adding '%s: %s' to label file, symbol: %s" % (texttokens[i],promvalue, symbol))
+            new_pl_line = first+"/K:"+promvalue+last
         else:
-            new_pl_line = pl_line+"/K:"+promvalues[i]
+            new_pl_line = pl_line+"/K:"+promvalue
         #print(new_pl_line)
         
         new_pl_lines.append(new_pl_line)
